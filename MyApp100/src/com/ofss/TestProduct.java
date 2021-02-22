adbb30c4c64692e3325cb74c42019e3ae4ccb049
Shashank18Git <shashankchityala@yahoo.co.in>
2021-02-22 16:00:58 +0530

    Second Commit to Project after adding Customer

--- MyApp100/src/com/ofss/TestProduct.java
+++ MyApp100/src/com/ofss/TestProduct.java
@@ -3,9 +3,14 @@ package com.ofss;
 public class TestProduct {
 
 	public static void main(String[] args) {
+		Stock [] stockArray = new Stock[3];
+		stockArray[0] = new Stock(100, "Oracle", 123.45);
+		stockArray[1] = new Stock(200, "IBM", 122.45);
+		stockArray[2] = new Stock(300, "Accenture", 121.45);
 		
-		Stock stock = new Stock(100, "Oracle", 123.45);
-		System.out.println(stock);
+		Customer customer = new Customer("Oracle", stockArray);
+		
+		System.out.println(customer);
 		System.out.println("----------------");
 
 	}
