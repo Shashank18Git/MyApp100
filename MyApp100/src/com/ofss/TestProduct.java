8117fbbe008de90a860c3b2a2b9ea8cca528b7f7
Shashank18Git <shashankchityala@yahoo.co.in>
2021-02-22 15:44:29 +0530

    First Commit of Maven Project from Eclipse

---
+++ MyApp100/src/com/ofss/TestProduct.java
@@ -0,0 +1,13 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		
+		Stock stock = new Stock(100, "Oracle", 123.45);
+		System.out.println(stock);
+		System.out.println("----------------");
+
+	}
+
+}
